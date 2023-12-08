91402d002bee47d9df8e813ea41991c4adc56616
Yang2 <Yang216099@163.com>
2023-12-08 21:24:44 +0800

    杨文杰-断点下载后端接口

--- src/main/java/com/example/atlasgeopre/controller/CommonController.java
+++ src/main/java/com/example/atlasgeopre/controller/CommonController.java
@@ -246,22 +246,21 @@ public class CommonController {
         return ResponseData.success();
     }
 
-    private final static String utf8 = "utf-8";
-
     @RequestMapping("/down")
+    @ApiOperation("断点下载")
     public void downLoadFile(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // 设置编码格式
-        response.setCharacterEncoding(utf8);
+        response.setCharacterEncoding("utf-8");
         //获取文件路径
         String fileName = request.getParameter("fileName");
-        String drive = request.getParameter("drive");
+//        String drive = request.getParameter("drive");
         //完整路径(路径拼接待优化-前端传输优化-后端从新格式化  )
-        String pathAll = drive + ":\\" + fileName;
+        String pathAll = "D:\\data\\cc\\" + fileName;
         Optional<String> pathFlag = Optional.of(pathAll);
         File file = null;
         //根据文件名，读取file流
         file = new File(pathAll);
-        System.out.println("文件路径");
+//        System.out.println("文件路径:" + file.getAbsoluteFile());
         if (!file.exists()) {
             System.out.println("文件不存在");
             return;
@@ -272,16 +271,16 @@ public class CommonController {
         try {
             //分片下载
             long fSize = file.length();//获取长度
-            response.setContentType("application/x-download");
+            response.setContentType("application/blob");
             String file_Name = URLEncoder.encode(file.getName(), "UTF-8");
-            response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.addHeader("Content-Disposition", "attachment;filename=" + file_Name);
             //根据前端传来的Range  判断支不支持分片下载
-            response.setHeader("Accept-Range", "bytes");
+            response.addHeader("Accept-Range", "bytes");
             //获取文件大小
             //response.setHeader("fSize",String.valueOf(fSize));
-            response.setHeader("fName", file_Name);
+            response.addHeader("fName", file_Name);
             //定义断点
-            long pos = 0, last = fSize - 1, sum = 0;
+            long pos = 0, last = fSize, sum = 0;
             //判断前端需不需要分片下载
             if (null != request.getHeader("Range")) {
                 response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
@@ -291,30 +290,30 @@ public class CommonController {
                     pos = Long.parseLong(strRange[0].trim());
                     last = Long.parseLong(strRange[1].trim());
                     //若结束字节超出文件大小 取文件大小
-                    if (last > fSize - 1) {
-                        last = fSize - 1;
+                    if (last > fSize) {
+                        last = fSize;
                     }
                 } else {
                     //若只给一个长度  开始位置一直到结束
                     pos = Long.parseLong(numRange.replaceAll("-", "").trim());
                 }
             }
-            long rangeLenght = last - pos + 1;
-            String contentRange = "bytes" + pos + "-" + last + "/" + fSize;
-            response.setHeader("Content-Range", contentRange);
-            // response.setHeader("Content-Lenght",String.valueOf(rangeLenght));
+            long rangeLenght = last - pos;
+            response.setHeader("Access-Control-Expose-Headers", "Content-Range");
+            String contentRange = "bytes " + pos + "-" + last + "/" + fSize;
+            response.addHeader("Content-Range", contentRange);
+            response.addHeader("Content-Length", String.valueOf(rangeLenght));
             os = new BufferedOutputStream(response.getOutputStream());
             is = new BufferedInputStream(Files.newInputStream(file.toPath()));
             is.skip(pos);//跳过已读的文件(重点，跳过之前已经读过的文件)
             byte[] buffer = new byte[1024];
             int lenght = 0;
-            //相等证明读完
+            System.out.println(pos + "-" + last);
             while (sum < rangeLenght) {
                 lenght = is.read(buffer, 0, (rangeLenght - sum) <= buffer.length ? (int) (rangeLenght - sum) : buffer.length);
                 sum = sum + lenght;
                 os.write(buffer, 0, lenght);
             }
-            System.out.println("下载完成");
         } finally {
             if (is != null) {
                 is.close();
